f1fae6acc91a40cec8eacfe34ec76718dfa701cb
Legion204 <ri46801@gmail.com>
2024-04-08 07:42:47 +0600

    added firebase registration,login,logout and hide the firebase keys

--- src/Firebase/Firebase.config.js
+++ src/Firebase/Firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAL0jTqV2trOmpsDopbCHKbb_nohp0gxZ4",
-  authDomain: "aura-real-estate.firebaseapp.com",
-  projectId: "aura-real-estate",
-  storageBucket: "aura-real-estate.appspot.com",
-  messagingSenderId: "335932395004",
-  appId: "1:335932395004:web:f8e6d83484fe22275cb679"
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain: import.meta.env.VITE_authDomain,
+  projectId: import.meta.env.VITE_projectId,
+  storageBucket: import.meta.env.VITE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId
 };
 
 // Initialize Firebase
